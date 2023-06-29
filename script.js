93ec83ede890e0f8f1b225efe14a5e5230ce75cf
brf153 <153hsb@gmail.com>
2023-06-29 20:03:39 +0530

    building weather api

--- script.js
+++ script.js
@@ -15,3 +15,30 @@ return e.text()
 })
 
 // weather api
+// api link https://weatherapi-com.p.rapidapi.com/current.json?q=25.3%2C83
+const weather = document.getElementById("weather")
+const weatherbox = document.getElementById("weather-box")
+const weatherinfo = document.getElementById("weather-info")
+const form = document.getElementById("weather-input")
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  
+  var inputField = document.getElementById("inputField");
+  var inputValue = inputField.value.trim();
+
+  if (inputValue !== "") {
+    var url = "backend-url?param=" + encodeURIComponent(inputValue);
+    window.location.href = url;
+  }
+});
+
+weatherbox.addEventListener("mouseover",(e)=>{
+  e.preventDefault()
+  document.getElementById("weather-input").style.display= "block"
+})
+
+weatherbox.addEventListener("mouseout",(e)=>{
+  e.preventDefault()
+  document.getElementById("weather-input").style.display = "none"
+})
