38e6fe06264a08e54fcadc3e6567c38ed9ac5665
brf153 <153hsb@gmail.com>
2023-07-01 11:57:16 +0530

    almost complete

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// onload func
+window.onload = function(){
+  let lat=25.3
+  let long=83
+  fetchData(lat,long)
+  getAuth()
+}
+
 // chess api
 const chess = document.getElementById("chess")
 
@@ -46,12 +54,6 @@ form.addEventListener("submit", function(event) {
 
 });
 
-window.onload = function(){
-  let lat=25.3
-  let long=83
-  fetchData(lat,long)
-}
-
 function fetchData(lat,long) {
   const options = {
     method: 'GET',
@@ -85,3 +87,47 @@ weatherbox.addEventListener("mouseout",(e)=>{
 
 // Shared Secret: 7768954ee663b6b9e149b7b911108a48
 
+const music_form = document.getElementById("music-input")
+
+music_form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  
+  var inputField = document.getElementById("music-inputField");
+  var inputValue = inputField.value.trim();
+  console.log(inputValue)
+
+  const options={
+    method: 'GET',
+    headers: { 'X-Api-Key': 'cDGXjpdViLH7pPMSq4qFmQ==TylhWViHqFmPapLK'},
+    contentType: 'application/json',
+  }
+
+    fetch(`https://api.api-ninjas.com/v1/geocoding?city=${inputValue}`,options)
+    .then((e)=> e.text())
+    .then((e)=>{
+      console.log(JSON.parse(e)[0].latitude)
+      let lat = JSON.parse(e)[0].latitude
+      let long = JSON.parse(e)[0].longitude
+      fetchData(lat,long)
+    })
+
+});
+
+function getAuth(){
+  const auth_key = "0be90c65e43fd06155c0d012cd8b0c0c"
+  const options = {
+    method: 'GET', // You can use 'POST', 'PUT', 'DELETE', etc. if needed
+    headers: {
+      'Origin': 'http://localhost:5500',
+    },
+    mode: 'cors', // No comma after 'cors'
+  }
+  fetch(`http://ws.audioscrobbler.com/2.0/?method=auth.gettoken&api_key=${auth_key}&format=json`)
+  .then((e)=>e.text()).then((e)=>{
+    console.log(JSON.parse(e))
+    const token = JSON.parse(e).token
+    fetch(`http://ws.audioscrobbler.com/2.0/?method=track.search&track=Choo&api_key=0be90c65e43fd06155c0d012cd8b0c0c&format=json`,options).then((e)=>e.text()).then((e)=>console.log(JSON.parse(e)))
+  })
+}
+// window.location.href= `http://www.last.fm/api/auth/?api_key=${auth_key}&token=${token}`
+// aKp-BsF0N2g8LiInxUBC7-70gjuRPVf_
